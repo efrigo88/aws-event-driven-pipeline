6f4500632a02e06e4ca9580b4b350d7f5bb305ff
Emiliano Frigo <emif.iron@gmail.com>
2025-06-12 14:35:21 +0200

    fixes and improvements

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -1,16 +1,31 @@
-resource "aws_cloudformation_stack" "pipe" {
-  name          = var.pipe_name
-  template_body = file("${path.module}/pipe.yaml")
-  capabilities  = ["CAPABILITY_NAMED_IAM"]
+resource "aws_pipes_pipe" "this" {
+  name     = var.pipe_name
+  role_arn = aws_iam_role.pipe_role.arn
+  source   = var.dynamodb_stream_arn
+  target   = var.sqs_queue_arn
 
-  parameters = {
-    PipeName          = var.pipe_name
-    PipeRoleArn       = aws_iam_role.pipe_role.arn
-    DynamoDBStreamArn = var.dynamodb_stream_arn
-    SQSQueueArn       = var.sqs_queue_arn
+  source_parameters {
+    dynamodb_stream_parameters {
+      starting_position = "LATEST"
+    }
+    filter_criteria {
+      filter {
+        pattern = jsonencode({
+          dynamodb = {
+            NewImage = {
+              status = { S = ["PENDING"] }
+            }
+          }
+        })
+      }
+    }
+  }
+
+  target_parameters {
+    sqs_queue_parameters {}
   }
 }
 
 output "pipe_arn" {
-  value = aws_cloudformation_stack.pipe.outputs["PipeArn"]
+  value = aws_pipes_pipe.this.arn
 }
